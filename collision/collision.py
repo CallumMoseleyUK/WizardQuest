fbb93b6b9e72786a1d9086a2419bdb24f2ec3e81
CallumMoseleyUK <Callum.Moseley.Work@gmail.com>
2025-05-30 17:55:14 +0100

    Collision culling added between models

--- collision/collision.py
+++ collision/collision.py
@@ -6,38 +6,44 @@ class CollisionManager:
     def __init__(self):
         pass
 
-    def apply_collisions(self,dt,ent_list):
-        for ent1 in ent_list:
-            for ent2 in ent_list:
-                if ent1==ent2 or not ent1.collision_model.check_collision(ent2.collision_model):
+    def apply_collisions(self,models):
+        model_list = models.copy()
+        for i,a in enumerate(model_list):
+            for b in model_list:
+                if not a or not b or a==b or a.cull_collision(b):
                     continue
-                displacement = ent1.position - ent2.position
-                distance_squared = np.dot(displacement, displacement)
-                
-
-                
-            
+                a.calculate_collision(b)
+            model_list[i] = None
 
 class CollisionModel:
     ''' A collection of primitives '''
     def __init__(self):
         super().__init__(self)
-        self.collision_primitives = []
-        self._bounding_geometry = BoundingBox(np.array([0.0,0.0,0.0]),np.array([0.0,0.0,0.0]))
+        self.primitives = []
+        self._bounding_geometry = None
 
     def add_primitive(self,primitive,bUpdate=False):
-        self.collision_primitives.append(primitive)
+        self.primitives.append(primitive)
         if bUpdate:
             self.update_bounding_geometry()
     
     def remove_primitive(self,primitive,bUpdate=False):
-        self.collision_primitives.remove(primitive)
+        self.primitives.remove(primitive)
         if bUpdate:
             self.update_bounding_geometry()
 
     def update_bounding_box(self):
-        for primitive in self.collision_primitives:
+        self._bounding_geometry = self.collision_primitives[0]._bounding_geometry
+        for primitive in self.collision_primitives[1:]:
             self._bounding_geometry = self._bounding_geometry.union(primitive._bounding_geometry)
+
+    def cull_collision(self,other):
+        return not self._bounding_geometry.check_intersects(other._bounding_geometry)
+    
+    def calculate_collision(self,other):
+        for a in self.primitives:
+            for b in other.primitives:
+                a.calculate_collision(b)
         
 
 
--- collision/primitive.py
+++ collision/primitive.py
@@ -3,16 +3,20 @@ from bounding_geometry import BoundingSphere
 
 class Primitive:
     def __init__(self):
-        pass
-    def check_collision(self,other):
-        pass
+        self._bounding_geometry = None
+    def cull_collision(self,other):
+        return self._bounding_geometry.check_intersects(other)
+    def calculate_collision(self,other):
+        return None
 
-class PrimitiveSphere:
-    ''' In this primitive, if boundingsphere check is passed, move straight to collision response, skipping boundingbox checks. '''
+class PrimitiveSphere(Primitive):
     def __init__(self, radius=1.0,offset=np.zeros(3)):
         self.radius = radius
+        self.offset = offset
         self._bounding_geometry = BoundingSphere(radius=radius,offset=offset)
-
-    def check_collision(self,other):
-        return self._bounding_geometry.check_intersects(other)
-
+    
+    def calculate_collision(self,other):
+        if self.cull_collision(other):
+            return None
+        distance_squared = np.dot(self.offset,other.offset)
+        # how to get the actual spatial position?
